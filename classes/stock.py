4dc452bba769b0732fe9eca57b69e0ebf7229362
tanermaddocks <maddockstaner@gmail.com>
2024-09-18 13:20:42 +1000

    class structures cont.

--- classes/stock.py
+++ classes/stock.py
@@ -10,14 +10,16 @@ class Spirit (Beverage):
     def __init__ (self, alcohol, cost, origin, type, subtype):
         super().__init__(alcohol, cost, type)
         self.origin = origin
-        self.subtype = subtype #scotch, fruit, dry
+        self.subtype = subtype #scotch, bourbon, etc.
 
 class Beer (Beverage):
     #constructor
-    def __init__(self, alcohol, cost, type):
+    def __init__(self, alcohol, cost, type, grain):
         super().__init__(alcohol, cost, type)
+        self.grain = grain
 
 class Wine (Beverage):
     #constructor
-    def __init__(self, alcohol, cost, type):
+    def __init__(self, alcohol, cost, type, fruit):
         super().__init__(alcohol, cost, type)
+        self.fruit = fruit
